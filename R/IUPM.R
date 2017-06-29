c0c77cc3201c95debb5a20234196cc52b9b7679a
Ilana Trumble <itrumble@unc.edu>
2017-06-29 21:09:57 +0000

    version 1.7

--- R/IUPM.R
+++ R/IUPM.R
@@ -1,6 +1,6 @@
 #' IUPM, PGOF, and CI
 #'
-#' Calculates the maximum likelihood estimate of infectious units per million (IUPM)
+#' Calculates the maximum likelihood estimate of concentration of infectious units
 #' from a single serial limiting dilution (SLD) assay. Also calculates corresponding
 #' exact and asymptotic confidence intervals, and a goodness-of-fit p-value. While
 #' this package was developed with the purpose of estimating IUPM, it is applicable to SLD
@@ -15,8 +15,10 @@
 #' more than 15,000 possible positive well outcomes exist, in which case 15,000 MC samples are taken.
 #'  Use monte=F for exact computation.
 #' @param conf.level Confidence level of the interval.
+#' @param iupm Boolean variable, indicates whether to return MLE as IUPM (TRUE) or probability a cell is infected (FALSE)
 #'
-#' @return \item{IUPM_MLE}{Maximum likelihood estimate of IUPM for the given outcome vector.}
+#' @return \item{MLE}{Maximum likelihood estimate for the given outcome vector.}
+#' @return \item{BC_MLE}{Bias corrected maximum likelihood estimate for the given outcome vector.}
 #' @return \item{Exact_PGOF}{P value for goodness of fit. PGOF is the probability of an
 #' experimental result as rare as or rarer than that obtained, assuming that the model is
 #' correct. Low values of PGOF, (e.g. PGOF < 0.01), indicate rare or implausible experimental
@@ -33,29 +35,36 @@
 #' # Duplicates row 4 of Table 4 from Myers, et. al.
 #' # Myers et. al. divides IUPM space into discrete values. This package searches
 #' # entire parameter space, yielding a slightly different and more accurate MLE.
-#' row4 <- get.iupm(pos=c(2,1,0,0,0,0),  # Number of positive wells per dilution level
+#' row4 <- get.mle(pos=c(2,1,0,0,0,0),  # Number of positive wells per dilution level
 #'                  replicates=rep(2,6), # Number of replicates per dilution level
 #'                  dilutions=c(1e6,2e5,4e4,8e3,1600,320), # Cells per dilution level
-#'                  conf.level=0.95   # Significance level
+#'                  conf.level=0.95,   # Significance level
+#'                  iupm=TRUE,            # Display MLE in infected units per million
 #'                  )
 #'
 #' # Duplicates row 21 of Table 4 from Myers, et. al.
 #' # Low PGOF example
 #' # Myers et. al. divides IUPM space into discrete values. This package searches
 #' # entire parameter space, yielding a slightly different and more accurate MLE.
-#' row21 <- get.iupm(pos=c(2,2,2,0,1,0),
+#' row21 <- get.mle(pos=c(2,2,2,0,1,0),
 #'                  replicates=rep(2,6),
 #'                  dilutions=c(1e6,2e5,4e4,8e3,1600,320),
-#'                  conf.level=0.95 )
+#'                  conf.level=0.95,
+#'                  iupm=TRUE)
+#'                  
+#' # Example calculating IUs per cell for an assay with 1 DL.
+#'  iu.example <- get.mle(pos=7, replicates=8, dilutions=25,
+#'                        conf.level=0.95, iupm=FALSE)
 #'
 #' # Monte Carlo example
 #' # 67,081 total possible positive well outcomes, therefore
 #' # Monte Carlo sampling is used to reduce computation time.
-#' MC.example <- get.iupm(pos=c(30,9,1,0),
+#' MC.example <- get.mle(pos=c(30,9,1,0),
 #'                        replicates=c(36,36,6,6),
 #'                        dilutions=c(2.5e6,5e5,1e5,2.5e4),
 #'                        conf.level=0.95,
-#'                        monte = 5000 )
+#'                        monte = 5000,
+#'                        iupm=TRUE )
 
 #' @import compiler
 #'
@@ -64,14 +73,49 @@
 # Calculate IUPM, PGOF, and CIs (main function)
 # Exact CI is default, only use Monte Carlo Sims if total rows is greater than 15000
 
-get.iupm <- function(pos,replicates,dilutions,monte=15000,conf.level=0.95){
+get.mle <- function(pos,replicates,dilutions,monte=15000,conf.level=0.95, iupm=TRUE){
+  
+  # Sanity checks
+  if (length(pos)!=length(replicates) | length(pos)!=length(dilutions) | length(replicates)!=length(dilutions)){
+    return("ERROR: vectors pos, replicates, and dilutions must be same length")
+  }
+  
+  if( all(pos<=replicates)==FALSE ){
+    return("ERROR: elements in pos must be less than or equal to replicates")    
+  }
+  
+  if ( all(pos>=0)==F | all(replicates>=0)==F | all(dilutions>=0)==F){
+    return("ERROR: elements in pos, replicates, and diltuionso must be non-negative")
+  }
+  
+  pos.int<-pos%%1
+  replicates.int<-replicates%%1
+  dilutions.int<-dilutions%%1
+  
+  if ( all(pos.int==0)==F | all(replicates.int==0)==F | all(dilutions.int==0)==F){
+    return("ERROR: elements in pos, replicates, and dilutions must be integers")
+  }
+  
+  monte.int<-monte%%1
+  if (monte.int!=0){
+    return("ERROR: Number of monte carlo samples must be integer")
+  }
+  
+  if(iupm!=T & iupm!=F){
+    return("ERROR: IUPM must be T or F")
+  }
 
   alpha <- 1-conf.level
   # Calculate MLE
   opt <- optim(-6,like,pos=pos,replicates=replicates,dilutions=dilutions,lower=-18,upper=14,method="Brent",hessian=T)
+  opt.gart <- suppressWarnings(optim(-6,like.gart,pos=pos,replicates=replicates,dilutions=dilutions,lower=-18,upper=14,method="Brent",hessian=F))
   mle <- exp(opt$par)
+  p.gart<-exp(opt.gart$par)/1e6
   se <- sqrt(1/opt$hessian)
-
+  
+  phat.gart<-p.gart-B.gart(p=p.gart,pos=pos,replicates=replicates,dilutions=dilutions)
+  mle.gart<-phat.gart*1e6
+  
   # Upper and lower asymptotic confidence intervals
   lower.asy <- exp(opt$par-qnorm(1-alpha/2)*se)
   upper.asy <- exp(opt$par+qnorm(1-alpha/2)*se)
@@ -88,49 +132,62 @@ get.iupm <- function(pos,replicates,dilutions,monte=15000,conf.level=0.95){
       val2=((E.i-O.i)^2)/(repsize[ii]-E.i)
       chisq=chisq+val1+val2
     }
-    df=length(n)-1
+    if ( length(n)-1==0 ){
+      df=1
+    }else{
+      df=length(n)-1
+    }
     pval=1-pchisq(chisq,df)
     return(pval)
   }
   pgof.asy<- asy.pvalue(n=dilutions,repsize=replicates,numsuccess=pos,f=mle/10^6)
 
   # Define number of Monte Carlo samples
-  if (monte==F) mc <- prod(replicates+1)
-  else mc <- min(monte,prod(replicates+1))
+  # subtract one because outcome will be added as MC sample later
+  if (monte==F) {
+    mc <- prod(replicates+1)
+    }else{
+      mc <- min(monte,prod(replicates+1))
+    }
+  
 
   # convert - randomly generate outcomes of positive wells "rows" times, without replacement
   rows <- sample(1:prod(replicates+1),mc,replace=F)
   crows <- convert(rows,replicates)
 
-  # MLEs for all outcomes
+  # MLEs for all sampled outcomes
   mles <- matrix(0,mc,1)
-  for (ii in 1:mc){
+  for (ii in 1:(mc)){
     opt <- optim(-6,like,pos=crows[ii,],replicates=replicates,dilutions=dilutions,lower=-18,upper=14,method="Brent")
     mles[ii] <- exp(opt$par)
   }
 
+   
   # PGOF
   gof.p <- gofp(pos,replicates,dilutions,mle,mles,crows,mc)
 
   # Exact Confidence Intervals
 
   # If p-value at 0 is not less than 0.05, set lower exact interval to 0
+  # If p-value at 0 is greater than 0.05, set lower exact interval to 0
+
   if (getp.alphac(0,pos,replicates,dilutions,mle,mles,crows,mc,alpha)>0){
     lower <- 0
   }else{
-    lower <- uniroot(getp.alphac,c(0,mle),pos,replicates,dilutions,mle,mles,crows,mc,alpha,extendInt="up")$root
+    lower <- uniroot(getp.alphac,c(0,mle),pos,replicates,dilutions,mle,mles,crows,mc,alpha,extendInt="up",maxiter=1000)$root
   }
 
   # If p-value at 1e6 is not less than 0.05, set upper exact interval to infinity
   # (We cannot say anything about the upper bound in this case)
-  if (getp.alphac(1e6,pos,replicates,dilutions,mle,mles,crows,mc,alpha)>0){
+  if (getp.alphac.log(log(1e6),pos,replicates,dilutions,mle,mles,crows,mc,alpha)>0){
     upper <- Inf
     mle <- Inf
     lower.asy= NA
     upper.asy= NA
   }else{
-    upper <- uniroot(getp.alphac,c(mle,1e6),pos,replicates,dilutions,mle,mles,crows,mc,alpha,extendInt="down")$root
-  }
+    logupper <- uniroot(getp.alphac.log,c(log(mle),log(1e6)),pos,replicates,dilutions,mle,mles,crows,mc,alpha,extendInt="down")$root
+    upper<-exp(logupper)
+    }
 
   # If Monte Carlo sampling is used, print sampling fraction
   if (mc<prod(replicates+1)){
@@ -145,5 +202,12 @@ get.iupm <- function(pos,replicates,dilutions,monte=15000,conf.level=0.95){
 
   asy=c(lower.asy,upper.asy)
   exact=c(lower,upper)
-  return(list("IUPM_MLE"=mle,"Exact_PGOF"=gof.p,"Exact_CI"=exact, "Asymp_PGOF"=pgof.asy,"Asymp_CI"=asy))
+  
+  if (iupm==F){
+    mle=mle/1e6
+    mle.gart=mle.gart/1e6
+    exact=exact/1e6
+    asy=asy/1e6
+  }
+  return(list("MLE"=mle,"BC_MLE"=mle.gart, "Exact_PGOF"=gof.p,"Exact_CI"=exact, "Asymp_PGOF"=pgof.asy,"Asymp_CI"=asy))
 }
--- R/helpers.R
+++ R/helpers.R
@@ -12,17 +12,94 @@ like <- function(logiupm,pos,replicates,dilutions){
   -ans
 }
 
+# For Gart: Log Likelihood, Fisher's information, first derivative of Fisher's information, and
+# expectation of third derivative of log likelihood
+
+like.gart<-function(logiupm,pos,replicates,dilutions){
+  iupm<-exp(logiupm)
+  ans <- 0
+  nlevels <- length(replicates)
+  p <- iupm/1e6
+  for (ii in 1:nlevels){
+    u<-dilutions[ii]
+    ans<- ans+ pos[ii]*log((1-(1-p)^u)/(1-p)^u) + (replicates[ii]*u)*log((1-p))
+  }
+  -ans
+}
+
+fisher.der.gart<-function(p,pos,replicates,dilutions){
+  ans<-0
+  nlevels<-length(replicates)
+  q<-1-p
+  for(ii in 1:nlevels){
+    k<-dilutions[ii]
+    n<-replicates[ii]
+    ans<-ans+ (-k^2*n*q^(k-3))*(k-2+2*q^k)/(1-q^k)^2
+  }
+  return(ans)
+}
+
+fisher.gart<-function(p,pos,replicates,dilutions){
+  ans<-0
+  nlevels<-length(replicates)
+  q<-1-p
+  for(ii in 1:nlevels){
+    k<-dilutions[ii]
+    n<-replicates[ii]
+    ans<-ans+ (k^2*n*q^(k-2))/(1-q^k)
+  }
+  return(ans)
+}
+
+third.der.gart<-function(p,pos,replicates,dilutions){
+  ans<-0
+  nlevels<-length(replicates)
+  q<-1-p
+  for(ii in 1:nlevels){
+    k<-dilutions[ii]
+    n<-replicates[ii]
+    ans<-ans+ (-k^2*n*q^(k-3))*(3-3*q^k-k-k*q^k)/(1-q^k)^2
+  }
+  return(ans)
+}
+
+B.gart<-function(p,pos,replicates,dilutions){
+  dIdP<-fisher.der.gart(p,pos,replicates,dilutions)
+  Ed3dp<-third.der.gart(p,pos,replicates,dilutions)
+  I<-fisher.gart(p,pos,replicates,dilutions)
+  ans<- -(2*dIdP+Ed3dp)/(2*I^2)
+  return(ans)
+}
+
 # For exact CI - produce outcome of positive wells from an integer (change of base)
 convert <- function(rows,replicates){
-  nlevels <- length(replicates)
-  nrows <- length(rows)
-  output <- matrix(0,nrows,nlevels)
+  # rows are the randomly selected row indices where one row is a dilution assay outcome
+  nlevels <- length(replicates) #number of dilution levels
+  nrows <- length(rows) #number of mc samples
+  output <- matrix(0,nrows,nlevels) #initialize matrix where each row is one assay outcome
   newrows <- rows
   for (ii in 1:nlevels){
-    output[,nlevels-ii+1] <- newrows%%(replicates[nlevels-ii+1]+1)
-    newrows <- (newrows-output[,nlevels-ii+1])/(replicates[nlevels-ii+1]+1)
+    # Ilana changed this
+    # original: output[,nlevels-ii+1] <- newrows%%(replicates[nlevels-ii+1]+1)
+    output[,ii] <- newrows%%(replicates[ii]+1)
+    
+    # Ilana changed this
+    # orig: newrows <- (newrows-output[,nlevels-ii+1])/(replicates[nlevels-ii+1]+1)
+    newrows <- (newrows-output[,ii])/(replicates[ii]+1)
   }
-  output
+  return(output)
+}
+
+iconvert<-function(crow,replicates){
+  #browser()
+  nlevels<-length(replicates)
+  nrow1<-0
+  for(ii in 1:nlevels){
+    nrow0<-(nrow1*(replicates[nlevels-ii+1]+1))+crow[nlevels-ii+1]
+    #print(nrow0)
+    nrow1<-nrow0
+  }
+  return(nrow1)
 }
 
 # Calculate the likelihood of a result given IUPM
@@ -38,6 +115,7 @@ like.long <-  function(iupm,pos,replicates,dilutions){
   ans
 }
 
+
 # LRT p-value - for confidence intervals
 # pobsj - probability of observed outcome at IUPM tauj, num
 # pobsmle - probability of observed outcome at MLE (tau hat), denom
@@ -46,16 +124,21 @@ like.long <-  function(iupm,pos,replicates,dilutions){
 # piis - denoms for relative likelihoods we are going to compare to ratio2
 # mc- number of MC simulations
 getp <- function(tauj,pos,replicates,dilutions,mle,mles,crows,mc){
+  if (tauj==mle){
+    return(1)
+  }
   ans <- 0
   posvec <- matrix(pos,1,length(dilutions))
-  pobsj <- 	like.long(tauj,		pos=posvec,replicates=replicates,dilutions=dilutions)
-  pobsmle <- 	like.long(mle,		pos=posvec,replicates=replicates,dilutions=dilutions)
+  pobsj <- 	like.long(tauj,	pos=posvec,replicates=replicates,dilutions=dilutions)
+  pobsmle <- 	like.long(mle, pos=posvec,replicates=replicates,dilutions=dilutions)
   ratio2 <- pobsj/pobsmle
-  pijs <- like.long(tauj,		pos=crows,replicates=replicates,dilutions=dilutions)
-  piis <- like.long(mles,		pos=crows,replicates=replicates,dilutions=dilutions)
+  pijs <- like.long(tauj,	pos=crows,replicates=replicates,dilutions=dilutions)
+  piis <- like.long(mles,	pos=crows,replicates=replicates,dilutions=dilutions)
   ratio1 <- pijs/piis
   index <- ratio1 <= ratio2
-  ans <- sum(pijs[index]) #sum probabilities where ratio less than observed obs's ratio
+  # It is possible for piis to be 0, thus causing ratio1 to be NA. The equality
+  # will not hold in these cases, so exclude NA's from the sum
+  ans <- sum(pijs[index], na.rm=T) #sum probabilities where ratio less than observed obs's ratio
   # slower version using loop
   # for (ii in 1:mc){
   #	crowsii <- matrix(crows[ii,],1,length(dilutions))
@@ -64,8 +147,9 @@ getp <- function(tauj,pos,replicates,dilutions,mle,mles,crows,mc){
   #	ratio1 <- pijs[ii]/pii
   #	if (ratio1<=ratio2) ans <- ans + pijs[ii]
   #	}
-  weight <- prod(replicates+1)/mc # weight for monte carlo, add 1 cuz need to count 0 case as option
-  ans*weight
+  weight <- (prod(replicates+1))/(mc) # weight for monte carlo, add 1 cuz need to count 0 case as option
+  sumweight<-ans*weight
+  return(sumweight)
 }
 
 # CMPfun - Speeds up code using just in time compiler
@@ -75,7 +159,12 @@ getpc <- cmpfun(getp)
 getp.alpha <- function(tauj,pos,replicates,dilutions,mle,mles,crows,mc,alpha){
   getpc(tauj,pos,replicates,dilutions,mle,mles,crows,mc)-alpha}
 
+getp.alpha.log<-function(logtauj,pos,replicates,dilutions,mle,mles,crows,mc,alpha){
+  tauj<-exp(logtauj)
+  getpc(tauj,pos,replicates,dilutions,mle,mles,crows,mc)-alpha}
+
 getp.alphac <- cmpfun(getp.alpha)
+getp.alphac.log<-cmpfun(getp.alpha.log)
 
 # GOF p-value - for PGOF
 gofp <- function(pos,replicates,dilutions,mle,mles,crows,mc){
@@ -91,6 +180,7 @@ gofp <- function(pos,replicates,dilutions,mle,mles,crows,mc){
   #	pimle <- like.long(mle,		pos=crowsii,replicates=replicates,dilutions=dilutions)
   #	if (pimle<=pobsmle) ans <- ans + pimle
   #	}
-  weight <- prod(replicates+1)/mc
-  ans*weight
+  weight <- (prod(replicates+1))/(mc)
+  weightsum<-ans*weight
+  return(weightsum)
 }
